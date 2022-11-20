0e02f5c0e55ea1bbaa6f4ef20e3d7e74f3501016
jobemcamera <jobecamera@gmail.com>
2022-11-20 10:48:55 -0300

    setas up e down mudando com javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,35 @@
-
-const dropDownFeatures = document.querySelector('#btn-features');
+// Features
 const featuresContent = document.querySelector('.features-dropdown');
-const arrowUp = document.querySelector('.arrow-up');
-const arrowDown = document.querySelector('.arrow-down');
-const dropDownCompany = document.querySelector('#btn-company');
+const arrowUpFeatures = document.querySelector('.arrow-up-features');
+const dropDownFeatures = document.querySelector('#btn-features');
+const arrowDownFeatures = document.querySelector('.arrow-down-features');
+// Company
 const companyContent = document.querySelector('.company-dropdown');
+const arrowUpCompany = document.querySelector('.arrow-up-company');
+const arrowDownCompany = document.querySelector('.arrow-down-company');
+const dropDownCompany = document.querySelector('#btn-company');
+
 
 dropDownFeatures.addEventListener('click', function() {
     if (featuresContent.style.display == 'block') {
         featuresContent.style.display = 'none';
+        arrowDownFeatures.style.display = 'none';
+        arrowUpFeatures.style.display = 'inline';
     } else {
         featuresContent.style.display = 'block';
+        arrowUpFeatures.style.display = 'none';
+        arrowDownFeatures.style.display = 'inline';
     }
 });
 
 dropDownCompany.addEventListener('click', function() {
     if (companyContent.style.display == 'block') {
         companyContent.style.display = 'none';
+        arrowDownCompany.style.display = 'none';
+        arrowUpCompany.style.display = 'inline';
     } else {
         companyContent.style.display = 'block';
+        arrowUpCompany.style.display = 'none';
+        arrowDownCompany.style.display = 'inline';
     }
 });
