0559a8412689663825da74f69558464feda9ac2f
jobemcamera <jobecamera@gmail.com>
2022-11-22 19:33:51 -0300

    abrir e fechar menu responsivo ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,23 +13,39 @@ const dropDownCompany = document.querySelector('#btn-company');
 dropDownFeatures.addEventListener('click', function() {
     if (featuresContent.style.display == 'block') {
         featuresContent.style.display = 'none';
-        arrowUpFeatures.style.display = 'none';
         arrowDownFeatures.style.display = 'inline';
+        arrowUpFeatures.style.display = 'none';   
     } else {
         featuresContent.style.display = 'block';
-        arrowDownFeatures.style.display = 'none';
         arrowUpFeatures.style.display = 'inline';
+        arrowDownFeatures.style.display = 'none';
+       
     }
 });
 
 dropDownCompany.addEventListener('click', function() {
     if (companyContent.style.display == 'block') {
         companyContent.style.display = 'none';
-        arrowUpCompany.style.display = 'none';
         arrowDownCompany.style.display = 'inline';
+        arrowUpCompany.style.display = 'none';
     } else {
         companyContent.style.display = 'block';
-        arrowDownCompany.style.display = 'none';
         arrowUpCompany.style.display = 'inline';
+        arrowDownCompany.style.display = 'none';
     }
 });
+
+const closeMenu = document.querySelector('.close-icon-menu');
+const openMenu = document.querySelector('.open-icon-menu');
+const menuBar = document.querySelector('.menu-bar');
+const loginRegister = document.querySelector('.login-register');
+
+openMenu.onclick = () => {
+    menuBar.classList.add('active');
+    loginRegister.classList.add('active');
+};
+
+closeMenu.onclick = () => {
+    menuBar.classList.remove('active');
+    loginRegister.classList.remove('active');
+};
